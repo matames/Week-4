02520d07f9cfc04033018ddb7634347e9da8ab8c
Ames <mat715@nyu.edu>
2021-09-27 21:01:31 -0400

    Re-skinned + animated sprites
    Used animator for sprite animation and changing and also redid all the sprites lol

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -9,6 +9,8 @@ public class PlayerMove : MonoBehaviour
     public float jumpHeight;
     public float gravityMultiplier;
     public float afterjumpVelocity;
+    public Transform spawnPoint;
+    public Transform winPoint;
 
     private float speedValue;
    
@@ -37,21 +39,29 @@ public class PlayerMove : MonoBehaviour
     {
         
 
-        if(onFloor && myBody.velocity.y > 0)
+        if(onFloor && myBody.velocity.y != 0)
         {
             onFloor = false;
         }
 
-        if(onFloor && myBody.velocity.x == 0)
+        if (onWall && myBody.velocity.y != 0)
+        {
+            onFloor = false;
+            anim.SetBool("On Wall", true);
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Walking", false);
+        }
+
+        if (onFloor && myBody.velocity.x == 0)
         {
             anim.SetBool("Walking", false);
         }
 
-        if(!onFloor && onWall)
+        if(myBody.velocity.y != 0)
         {
             anim.SetBool("Walking", false);
-            myRenderer.sprite = wallSprite;
         }
+
 
         CheckKeys();
         JumpPhysics();
@@ -62,8 +72,14 @@ public class PlayerMove : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D))
         {
+            if(myBody.velocity.y == 0)
+            {
+                anim.SetBool("Jumping", false);
+                anim.SetBool("On Wall", false);
+                anim.SetBool("Walking", true);
+            }
+
             myRenderer.flipX = false;
-            anim.SetBool("Walking", true);
             HandleLRMovement(speed);
             speedValue = 1;
             //myBody.velocity += Vector2.right * Physics2D.gravity * (accelerationMultiplier - 1f) * Time.deltaTime;
@@ -71,25 +87,40 @@ public class PlayerMove : MonoBehaviour
 
         } else if (Input.GetKey(KeyCode.A))
         {
+            if (myBody.velocity.y == 0)
+            {
+                anim.SetBool("Jumping", false);
+                anim.SetBool("On Wall", false);
+                anim.SetBool("Walking", true);
+            }
             myRenderer.flipX = true;
-            anim.SetBool("Walking", true);
             HandleLRMovement(-speed);
             speedValue = -1;
             //myBody.velocity += Vector2.left * -Physics2D.gravity * (accelerationMultiplier - 1f) * Time.deltaTime;
 
         }
 
-        if(Input.GetKeyDown(KeyCode.W) && onFloor || Input.GetKeyDown(KeyCode.W) && onWall)
+        if(Input.GetKeyDown(KeyCode.W) && onFloor)
         {
             anim.SetBool("Walking", false);
-            myRenderer.sprite = jumpSprite;
+            anim.SetBool("Jumping", true);
+            myBody.velocity = new Vector3(myBody.velocity.x, jumpHeight);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.W) && onWall)
+        {
+            anim.SetBool("Walking", false);
+            anim.SetBool("Jumping", false);
+            anim.SetBool("On Wall", true);
             myBody.velocity = new Vector3(myBody.velocity.x, jumpHeight);
 
         }
 
 
-        else if(!Input.GetKeyDown(KeyCode.W) && !onFloor)
+        else if (!Input.GetKeyDown(KeyCode.W) && !onFloor)
         {
+            anim.SetBool("Jumping", true);
             myBody.velocity += Vector2.up * (Physics2D.gravity.y) / 4 * (jumpHeight - 1f) * Time.deltaTime;
         }
     }
@@ -98,7 +129,10 @@ public class PlayerMove : MonoBehaviour
     {
         if(myBody.velocity.y < 0)
         {
-            anim.SetBool("Walking", false);
+            if (!onWall)
+            {
+                anim.SetBool("Jumping", true);
+            }
             myBody.velocity += Vector2.up * Physics2D.gravity.y * (gravityMultiplier - 1f) * Time.deltaTime;
         } 
     }
@@ -113,20 +147,31 @@ public class PlayerMove : MonoBehaviour
     {
         if(collision.gameObject.tag == "floor")
         {
-            myRenderer.sprite = walkSprite;
+            anim.SetBool("Jumping", false);
+            anim.SetBool("On Wall", false);
             onFloor = true;
-            myBody.velocity = new Vector2(speedValue * afterjumpVelocity, myBody.velocity.y);
+            myBody.velocity = new Vector2(speedValue/2 * afterjumpVelocity, myBody.velocity.y);
 
         }
         if(collision.gameObject.tag == "enemy")
         {
-            Destroy(gameObject);
+           gameObject.transform.position = spawnPoint.transform.position;
+            myBody.velocity = new Vector3(0, 0);
         }
 
         if(collision.gameObject.tag == "wall")
         {
+            anim.SetBool("On Wall", true);
             anim.SetBool("Walking", false);
+            anim.SetBool("Jumping", false);
             onWall = true;
         }
+
+        if(collision.gameObject.tag == "win")
+        {
+            gameObject.transform.position = winPoint.transform.position;
+            myBody.velocity = new Vector3(0, 0);
+        }
+
     }
 }
